4832a23b29a9d61b6a14dc264e8209f6d80c283d
Daniel Lee <dlee@daniels-mbp.home>
2021-01-12 18:57:26 -0800

    Set debug value

--- spacehut/settings.py
+++ spacehut/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SPACEHUT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('SPACEHUT_DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['spacehut.herokuapp.com']
 
