893ac1d9ffeb0aa5888a283d2256c7fc349659e9
Daniel Lee <dlee@daniels-mbp.home>
2021-01-13 19:20:22 -0800

    Configured asgi

--- spacehut/asgi.py
+++ spacehut/asgi.py
@@ -12,6 +12,5 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spacehut.settings')
-os.environ['ASGI_THREADS']="4"
 django.setup()
 application = get_default_application()
